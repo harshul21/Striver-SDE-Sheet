e06aba94efe0f96130c4c42413c7ef01d9a03a40
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-02-02 23:39:44 +0530

    Update Count Inversion.cpp

--- Count Inversion.cpp
+++ Count Inversion.cpp
@@ -1,1 +1,58 @@
-
+//Problem Link:-https://www.codingninjas.com/codestudio/problems/count-inversions_615?topList=striver-sde-sheet-problems&leftPanelTab=1
+//Youtube Link:-https://www.youtube.com/watch?v=kQ1mJlwW-c0&t=47s
+long long merge(long long *arr,long long *temp,int left,int mid,int right)
+{
+    int i,j,k;
+    i=left;
+    j=mid;
+    k=left;
+    int inv_count=0;
+    while(i<=mid-1 and j<=right)
+    {
+        if(arr[i]<=arr[j])
+        {
+            temp[k++]=arr[i++];
+        }
+        else
+        {
+            temp[k++]=arr[j++];
+            inv_count=inv_count+(mid-i);
+        }
+    }
+    while(i<=mid-1)
+    {
+        temp[k++]=arr[i++];
+    }
+    while(j<=right)
+    {
+        temp[k++]=arr[j++];
+    }
+    for(int i=left;i<=right;i++)
+    {
+        arr[i]=temp[i];
+    }
+    return inv_count;
+}
+long long mergesort(long long *arr,long long *temp,int left,int right)
+{
+    long long inv_count=0;
+    if(right>left)
+    {
+        int mid=left+(right-left)/2;
+        inv_count+=mergesort(arr,temp,left,mid);
+        inv_count+=mergesort(arr,temp,mid+1,right);
+        inv_count+=merge(arr,temp,left,mid+1,right);
+    }
+    
+    return inv_count;
+}
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    if(n==0)
+    {
+        return 0;
+    }
+    long long temp[n];
+    long long ans=mergesort(arr,temp,0,n-1);
+    return ans;
+}
