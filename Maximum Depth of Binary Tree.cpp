20138c509fdb6cbb0b1c024e1c7e286efe0c8038
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-02-05 23:50:15 +0530

    Create Maximum Depth of Binary Tree.cpp

---
+++ Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,37 @@
+//Problem Link:-https://www.codingninjas.com/codestudio/problems/identical-trees_799364?topList=striver-sde-sheet-problems&leftPanelTab=0
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    // Write your code here. 
+    if(root1==NULL and root2==NULL){
+        return true;
+    }
+    if(root1==NULL or root2==NULL){
+        return false;
+    }
+    if(root1->data!=root2->data){
+        return false;
+    }
+    
+    return identicalTrees(root1->left,root2->left) and identicalTrees(root1->right,root2->right);
+        
+}
